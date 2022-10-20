e1b56819dd84f450b76d36efe4f6129e5b4d8be8
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-10-20 16:18:41 +0400

    Add project files.

---
+++ Containers/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Containers;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Button btn = new()
+        {
+            Content = 4,
+            FontSize = 42
+        };
+
+        // sp.Children.Add(btn);
+
+    }
+}
